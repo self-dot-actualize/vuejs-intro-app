83cd9ec0d82ea9593739cdead16209ee71d5b554
Peter Jang <peter.x.jang@gmail.com>
2017-04-23 09:36:41 -0500

    Initial commit

---
+++ app/assets/javascripts/pages.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function(event) { 
+  var app = new Vue({
+    el: '#app',
+    data: {
+      message: 'Hello Vue!',
+      name: "Peter"
+    }
+  });
+});
